e12d12469613f2a5fb7aa64adc86e8f74abfb534
lipsa100 <73013154+lipsa100@users.noreply.github.com>
2021-05-26 11:40:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,35 +15,42 @@ recentbutton(buttonInnerHTML);
    function makesound(key){
   switch (key){
         case "w":
-          var tom1 = new Audio("tom-1.mp3");
-          tom1.play();
+//           var tom1 = new Audio("tom-1.mp3");
+//           tom1.play();
+            alert("Hello W");
         break;
         case "a":
-          var tom2 = new Audio("tom-2.mp3");
-          tom2.play();
+//           var tom2 = new Audio("tom-2.mp3");
+//           tom2.play();
+            alert("Hello A");
         break;
         case "s":
-          var tom3 = new Audio("tom-3.mp3");
-          tom3.play();
+//           var tom3 = new Audio("tom-3.mp3");
+//           tom3.play();
+            alert("Hello S");
         break;
         case "d":
-          var tom4 = new Audio("tom-4.mp3");
-          tom4.play();
+//           var tom4 = new Audio("tom-4.mp3");
+//           tom4.play();
+            alert("Hello D");
         break;
         case "j":
-          var snare = new Audio("snare.mp3");
-          snare.play();
+//           var snare = new Audio("snare.mp3");
+//           snare.play();
+            alert("Hello J");
         break;
         case "k":
-          var crash = new Audio("crash.mp3");
-          crash.play();
+//           var crash = new Audio("crash.mp3");
+//           crash.play();
+            alert("Hello K");
         break;
         case "l":
-          var kick = new Audio("kick-bass.mp3");
-          kick.play();
+//           var kick = new Audio("kick-bass.mp3");
+//           kick.play();
+            alert("Hello L");
         break; 
         default:
-        alert("invalid");
+        alert("wrong entry");
         break;
       }
     });
